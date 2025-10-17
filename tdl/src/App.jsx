92c8fac93e878360b389ca47076e4952bbb7343b
Ssnig <kyawsanh402@gmail.com>
2025-10-17 17:30:32 +0630

    ff

--- tdl/src/App.jsx
+++ tdl/src/App.jsx
@@ -1,15 +1,27 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
 import Form from './components/Form';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import List from './components/List';
 
+
 function App() {
+  const fetchedata = async () => {
+    const data = await fetch('http://localhost:4000/todolist ')
+      .then(response => response.json())
+      .catch(error => console.log("Error", error))
+    console.log(data);
+  }
+  
+  useEffect(() => {
+    fetchedata();
+  }, [])
+
   return (
     <div className='mx-auto  w-50' >
-    
-      <List/>
-      
+
+      <List />
+
     </div>
   );
 }
