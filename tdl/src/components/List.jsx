9209b6f24728f445d8321646ae3ccb1986489438
Ssnig <kyawsanh402@gmail.com>
2025-10-17 16:14:25 +0630

    dd

--- tdl/src/components/List.jsx
+++ tdl/src/components/List.jsx
@@ -14,27 +14,7 @@ const List = () => {
                     </div>
 
                 </div>
-                <div className='list-group-item w-100 mt-3 shadow-sm' >
-                    <div className="row">
-                        <div className="col-10">Task Message</div>
-                        <div className="col-2 " ><i class="fa-solid fa-trash"></i></div>
-                    </div>
-
-                </div>
-                <div className='list-group-item w-100 mt-3 shadow-sm' >
-                    <div className="row">
-                        <div className="col-10">Task Message</div>
-                        <div className="col-2 " ><i class="fa-solid fa-trash"></i></div>
-                    </div>
-
-                </div>
-                <div className='list-group-item w-100 mt-3 shadow-sm bg-danger text-white' >
-                    <div className="row">
-                        <div className="col-10">Task Message</div>
-                        <div className="col-2 " ><i class="fa-solid fa-trash"></i></div>
-                    </div>
-
-                </div>
+                
 
             </ol>
         </div>
